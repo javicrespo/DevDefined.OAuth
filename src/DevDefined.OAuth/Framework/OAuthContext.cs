9b1d5994f8b2005636f737c4dca6238a96236b80
javicrespo <javiercrespoalvez@gmail.com>
2011-03-04 02:21:13 -0800

    Missing using bug

--- src/DevDefined.OAuth/Framework/OAuthContext.cs
+++ src/DevDefined.OAuth/Framework/OAuthContext.cs
@@ -27,6 +27,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using System.Linq;
 using System.Text;
 using System.Web;
